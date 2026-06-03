997ea5116c0c83f2c0b41404473a5e42a8d8dbcc
Hüseyin Gökay Benian <creatorgky@outlook.com>
2026-06-03 21:44:59 +0300

    Anim Instance

---
+++ Source/CppFpsProject/Private/Animation/TestAnimInstance.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/TestAnimInstance.h"
+
+#include "KismetAnimationLibrary.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UTestAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PawnOwner = TryGetPawnOwner();
+	if (PawnOwner)
+	{
+		if (ACharacter* CharacterOwner = Cast<ACharacter>(PawnOwner)) CharacterMovement = CharacterOwner->GetCharacterMovement();
+	}
+}
+
+void UTestAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{                                 
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (PawnOwner)
+	{
+		Speed = PawnOwner->GetVelocity().Size();
+		Direction =	UKismetAnimationLibrary::CalculateDirection(PawnOwner->GetVelocity(), PawnOwner->GetActorRotation());
+		if (CharacterMovement)
+		{
+			bIsFalling = CharacterMovement->IsFalling();
+			CharacterMovement->GetCurrentAcceleration() != FVector::ZeroVector && Speed>0.f ? bShouldMove = true : bShouldMove = false;
+		}	
+	}
+}
---
+++ Source/CppFpsProject/Public/Animation/TestAnimInstance.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TestAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class CPPFPSPROJECT_API UTestAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY()
+	UCharacterMovementComponent* CharacterMovement;
+
+	UPROPERTY()
+	APawn* PawnOwner;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	float Direction;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bIsFalling;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bShouldMove;
+};
